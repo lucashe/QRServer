cfff0391921261c72a00e3ffed387aa1cb5dce83
LucasHe <lucas.he.sg@gmail.com>
2014-03-11 22:06:45 +0800

    Create emtpy project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QRServer::Application.config.secret_key_base = '8653c6c3177e7a6e9d6afc0b655e038218d901717689ffad128902cb805ebd18693592568a7b44ccf10c549cc0208fc74de741981b66606b956e65bb50687028'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QRServer::Application.config.session_store :cookie_store, key: '_QRServer_session'
